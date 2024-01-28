7478517c7cd4cb7758cc5ab286da6f0f3653eaf1
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2024-01-28 19:46:58 +0100

    no cache style added in login

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if (isset($_SESSION["user_name"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="./assets/images/dos.png" type="image/png">
-    <link rel="stylesheet" href="./assets/styles/styles.css">
+    <link rel="stylesheet" href="./assets/styles/styles.css?no-cache=<?php echo time(); ?>">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="./assets/scripts/notifications.js"></script>
     <script src="./assets/scripts/login.js"></script>
