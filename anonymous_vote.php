502d73440da5d1ebe99841e9dd63bdd8a5b36254
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2024-02-01 03:55:14 +0100

    fixed anonymous vote

--- anonymous_vote.php
+++ anonymous_vote.php
@@ -100,8 +100,28 @@ elseif (isset($_POST["opcion"]) && isset($_POST["token"]) && isset($_POST["email
         die("Error accedint a dades: " . $e[2]);
     }
     // seguir aqui lo de crear un usuario anonimo. Ten en cuenta que has cambiado el .sql de User y le has añadido un nuevo campo
-
     
+
+    echo "<!DOCTYPE html>";
+    echo "<html lang='es'>";
+    echo "<head>";
+    echo "    <meta charset='UTF-8'>";
+    echo "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+    echo "    <link rel='icon' href='./assets/images/dos.png' type='image/png'>";
+    echo "    <link rel='stylesheet' href='./assets/styles/styles.css'>";
+    echo "    <script src='https://code.jquery.com/jquery-3.6.4.min.js'></script>";
+    echo "    <script src='./assets/scripts/notifications.js'></script>";
+    echo "    <script src='./assets/scripts/anonymous_vote.js'></script>";
+    echo "    <title>Votar como Invitado</title>";
+    echo "</head>";
+    echo "<body>";
+    include("./templates/header.php");
+    echo "<h1>Tu voto ha sido guardado.</h1>";
+    echo "<h2>Registrate para ver los resultados:</h2>";
+    echo "<a href='/register.php'>Registrarse</a>";
+    include("./templates/footer.php");
+    echo "</body>";
+    echo "</html>";
 
 } else {
     include("./error404.php");
