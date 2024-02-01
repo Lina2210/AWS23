6a5ff58e05b97504b5f656ef3b139cbc1451d295
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2024-02-01 04:23:46 +0100

    test proxmox

--- anonymous_vote.php
+++ anonymous_vote.php
@@ -81,16 +81,16 @@ if (isset($_GET["token"]) && $_GET["token"] != 'ko') {
 } 
 elseif (isset($_POST["opcion"]) && isset($_POST["token"]) && isset($_POST["email"]) && isset($_POST["survey_id"])) {
     // borrar el token de invited user para no poder volver a votar y crear ese usuario en la tabla User para que cuando se registre que coja eso y se lo guarde.
-    $query = $pdo->prepare("UPDATE InvitedUser SET token = ? WHERE token = ?");
-    $query->bindParam(1, 'ko', PDO::PARAM_STR);
-    $query->bindParam(2, $_POST["token"], PDO::PARAM_STR);
-    $query->execute();
+    /*$queryUpdate = $pdo->prepare("UPDATE InvitedUser SET token = ? WHERE token = ?");
+    $queryUpdate->bindParam(1, 'ko', PDO::PARAM_STR);
+    $queryUpdate->bindParam(2, $_POST["token"], PDO::PARAM_STR);
+    $queryUpdate->execute();
 
-    $e = $query->errorInfo();
+    $e = $queryUpdate->errorInfo();
     if ($e[0] != '00000') {
         echo "\nPDO::errorInfo():\n";
         die("Error accedint a dades: " . $e[2]);
-    }
+    }*/
     // seguir aqui lo de crear un usuario anonimo. Ten en cuenta que has cambiado el .sql de User y le has añadido un nuevo campo
 
 
