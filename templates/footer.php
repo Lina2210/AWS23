bb84843b4d2ef062407b745348af848ea71706d7
claudiaSinMovil <cmoyanoherrerias.cf@iesesteveterradas.cat>
2024-02-02 18:32:24 +0100

    6

--- templates/footer.php
+++ templates/footer.php
@@ -3,6 +3,7 @@
         <li><img src="/assets/images/email-icon.svg">Lina Ramírez - lramirezhernandez.cf@iesesteveterradas.cat</li>
         <li><img src="/assets/images/email-icon.svg">Eric Escrich - eescrichalmagro.cf@iesesteveterradas.cat</li>
         <li><img src="/assets/images/email-icon.svg">Isaac Furió - ifuriomartin.cf@iesesteveterradas.cat</li>
+        <li><img src="/assets/images/email-icon.svg">Claudia Moyano - cmoyanoherrerias.cf@iesesteveterradas.cat</li>
     </ul>
     <div class="copyright">
         <a href="https://www.iesesteveterradas.cat/" target="_blank"><img src="/assets/images/link-icon.svg">INSTITUT ESTEVE TERRADAS I ILLA</a>
