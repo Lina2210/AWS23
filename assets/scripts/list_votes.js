0c160a4708911eed95e4c47907e4a96e1755d13b
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2024-02-07 00:52:28 +0100

    test list votes

--- assets/scripts/list_votes.js
+++ assets/scripts/list_votes.js
@@ -4,6 +4,7 @@ $(function() {
 
     $(".toggle-response").click(function() {
         var h3Element = $(this).closest("h3");
+        console.log(h3Element);
 
         if (h3Element.attr('hidden')) {
             h3Element.show();
